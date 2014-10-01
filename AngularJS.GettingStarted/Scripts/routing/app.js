58227f09771c55d8b9571f8c673a415279c62cea
Freeflyjunkie <erictorres56@gmail.com>
2014-10-01 17:40:02 -0400

    Routing

---
+++ AngularJS.GettingStarted/Scripts/routing/app.js
@@ -0,0 +1,18 @@
+﻿(function () {
+
+    var app = angular.module("githubViewer", ["ngRoute"]);
+
+    console.log("routing");
+
+    app.config(function ($routeProvider) {
+        $routeProvider          
+            .when('/', {
+                templateUrl: 'html/main.html',
+                controller: 'MainController'
+            })
+            .otherwise({ redirectUrl: "/Html/main" });
+
+        // route for the home page        
+    });
+
+}());
---
+++ AngularJS.GettingStarted/Scripts/routing/main.js
@@ -0,0 +1,7 @@
+﻿(function () {
+    angular.module('githubViewer').controller("MainController", ["$scope", 
+        function ($scope) {
+            console.log("in main controller");
+            $scope.message = "Getting Started With GitHub Viewer!";
+        }]);
+}());
